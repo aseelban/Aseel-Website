890ab402762a576afee4480cb91142df694bc9a9
Aseel Ban <aseelban@Aseels-MacBook-Pro.local>
2023-04-05 03:15:36 +0300

    Fix line-hight for home + align content for about component in-Mobile

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5176135aab0a704f"; }
+/******/ 		__webpack_require__.h = function() { return "d2793f839b37d1d8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
